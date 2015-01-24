a392df0a5090ba58f8991f2d5ee300989b8b7302
Daniel Renteria <df.renteria10@uniandes.edu.co>
2015-01-24 11:55:17 -0500

    Version 0.3 PSP0: Calculos

---
+++ src/main/java/edu/uniandes/ecos/psp0/StatisticCalculator.java
@@ -0,0 +1,64 @@
+package edu.uniandes.ecos.psp0;
+
+import java.util.LinkedList;
+
+/**
+ * 
+ * @author Daniel Felipe
+ * This class performs the statistic calculations
+ * (Mean and Standard Deviation)
+ *
+ */
+public class StatisticCalculator {
+
+	private LinkedList<Double> inputData;
+	
+	/**
+	 * Public constructor
+	 */
+	public StatisticCalculator()
+	{
+		inputData = new LinkedList<Double>();
+	}
+	
+	/**
+	 * Sets the data (numbers list) to be used to calculate
+	 * @param input LinkedList with the input data (numbers)
+	 */
+	public void setInputData(LinkedList<Double> input)
+	{
+		this.inputData = input;
+	}
+	
+	/**
+	 * This method calculates the mean of a list of numbers
+	 * @return The mean of a set of numbers or 0 if the list is empty
+	 */
+	public double calculateMean(){
+		double mean = 0;
+		double elementsSum = 0;
+		for(Double singleData : inputData){
+			elementsSum += singleData;
+		}
+		mean = elementsSum / inputData.size();
+		return mean;
+	}
+	
+	/**
+	 * This method calculates the standard deviation of a list
+	 * of numbers 
+	 * @return The standard deviation of the list of numbers or 0 if the
+	 * list is empty
+	 */
+	public double calculateStdDev(){
+		double stdDev = 0;
+		double mean = calculateMean();
+		double variationSum = 0;
+		for(Double singleData : inputData){
+			variationSum += Math.pow((singleData - mean), 2);
+		}
+		stdDev = Math.sqrt(variationSum / (inputData.size() - 1));
+		return stdDev;
+	}
+	
+}
---
+++ src/test/java/edu/uniandes/ecos/psp0/StatisticCalculatorTest.java
@@ -0,0 +1,85 @@
+package edu.uniandes.ecos.psp0;
+
+import java.util.LinkedList;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class StatisticCalculatorTest extends TestCase {
+	
+	/**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public StatisticCalculatorTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( StatisticCalculatorTest.class );
+    }
+    
+    private LinkedList<Double> getTestList1(){
+    	LinkedList<Double> inputs = new LinkedList<Double>();
+    	inputs.add(160D);
+    	inputs.add(591D);
+    	inputs.add(114D);
+    	inputs.add(229D);
+    	inputs.add(230D);
+    	inputs.add(270D);
+    	inputs.add(128D);
+    	inputs.add(1657D);
+    	inputs.add(624D);
+    	inputs.add(1503D);
+    	return inputs;
+    }
+    
+    private LinkedList<Double> getTestList2(){
+    	LinkedList<Double> inputs = new LinkedList<Double>();
+    	inputs.add(15.0D);
+    	inputs.add(69.9D);
+    	inputs.add(6.5D);
+    	inputs.add(22.4D);
+    	inputs.add(28.4D);
+    	inputs.add(65.9D);
+    	inputs.add(19.4D);
+    	inputs.add(198.7D);
+    	inputs.add(38.8D);
+    	inputs.add(138.2D);
+    	return inputs;
+    }
+    
+    /**
+     * Test for Mean Calculations
+     */
+    public void testMeanCalc(){
+    	StatisticCalculator calc = new StatisticCalculator();
+    	//First Set of Inputs
+    	calc.setInputData(getTestList1());
+    	assertEquals(550.6D, calc.calculateMean(), 1e-8D);
+    	//Second set of inputs
+    	calc.setInputData(getTestList2());
+    	assertEquals(60.32D, calc.calculateMean(), 1e-8D);
+    }
+    
+    /**
+     * Test for Standard Deviation Calculations
+     */
+    public void testStdDevCalc(){
+    	StatisticCalculator calc = new StatisticCalculator();
+    	//First Set of Inputs
+    	calc.setInputData(getTestList1());
+    	assertEquals(572.03D, calc.calculateStdDev(), 0.01);
+    	//Second set of inputs
+    	calc.setInputData(getTestList2());
+    	assertEquals(62.26D, calc.calculateStdDev(), 0.01);
+    }
+
+}
